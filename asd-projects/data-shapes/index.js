21a0395d4abf001e3119b3591798f9092a22685a
daviduzcategui44 <duzcategui924@gmail.com>
2026-02-03 19:00:20 +0000

    saving data shapes progress

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
 
   // TODO 3-a: add a function that handles the static display type
   function handleStatic(data) {
-    setBackgroundWithObject;
+    setBackgroundWithObject();
     animationDetails.displayType = 1;
   }
   // TODO 4-a: add a function that handles the good display type
@@ -69,6 +69,7 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
+    handleStatic();
   }
 
   function goodDisplay() {
