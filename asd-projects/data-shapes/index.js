5ac628473c966ea1a482e6b8c2d282f248b5582e
daviduzcategui44 <duzcategui924@gmail.com>
2026-02-09 18:37:10 +0000

    completed data shapes project

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -66,6 +66,10 @@ $(document).ready(function () {
   }
 
   // TODO 5-a: add a function that handles the bad display type
+  function handleBad(data, repeat) {
+    setBackgroundWithMixed(data, repeat = 1);
+    animationDetails.displayType = 3;
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -78,12 +82,15 @@ $(document).ready(function () {
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    var currentShape = (dataShapes[currentIndex]);
+    var currentShape = dataShapes[currentIndex];
     handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat;
+    handleBad(currentShape, repeat);
   }
 
   /////////////////////////////////////////////////
